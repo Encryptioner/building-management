04e429300eff53c6817db744ad9ce4bde4da7ec8
Ankur Mursalin <mir.ankur.ruet13@gmail.com>
2025-11-19 07:38:03 +0600

    feat: remove unused library

--- src/lib/pdf-generator/adapters/vue/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * PDF Generator - Vue 3 Adapter
- *
- * Vue 3 composables for PDF generation
- */
-
-export { usePDFGenerator, usePDFGeneratorManual } from './usePDFGenerator';
-export type { UsePDFGeneratorOptions, UsePDFGeneratorReturn } from './usePDFGenerator';
